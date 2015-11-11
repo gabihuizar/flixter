e280485498e86a876a83d276572cd18b7d9b0e73
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-11-11 00:01:24 +0000

    added course validations

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "instructor includes all parameters" do
+  	course = Course.create(title: 'Intro to Life', description: 'Learn all about life', cost: 10.00)
+  	assert course.valid?
+  	assert_redirect_to instructor_course_path(assigns(course))
+  end
+
+  test "instructor doesn't include all parameters" do 
+  	course = Course.create(title: 'Intro to Life')
+  	assert_not course.valid?
+  	
 end
