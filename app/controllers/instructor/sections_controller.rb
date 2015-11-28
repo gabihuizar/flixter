14cbc7b3b18ab4ff068318a93934f68868508df4
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-11-28 22:14:02 +0000

    sections new modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,15 +1,12 @@
 class Instructor::SectionsController < ApplicationController
 	before_action :authenticate_user!, :only => [:create]
-	before_action :require_authorized_for_current_course, :only => [:new, :create]
+	before_action :require_authorized_for_current_course, :only => [:create]
 	before_action :require_authorized_for_current_section, :only => [:update]
-
-	def new
-		@section = Section.new
-	end
 
 	def create
 		@section = current_course.sections.create(section_params)
 		redirect_to instructor_course_path(current_course)
+
 	end
 
 	def update
@@ -19,10 +16,22 @@ class Instructor::SectionsController < ApplicationController
 
 	private
 
+
+	def require_authorized_for_current_course
+		if current_course.user != current_user
+			render :text => "Unauthorized", :status => :Unauthorized
+		end
+	end
+
 	def require_authorized_for_current_section
 		if current_course.user != current_user
 			render :text => "Unauthorized", :status => :unauthorized
 		end
+	end
+
+	helper_method :current_course
+	def current_course
+		@current_course ||= Course.find(params[:course_id])
 	end
 
 	helper_method :current_section
@@ -30,16 +39,7 @@ class Instructor::SectionsController < ApplicationController
 		@current_section ||= Section.find(params[:id])
 	end
 
-	def require_authorized_for_current_course
-		if current_course.user != current_user
-			render :text => "Unauthorized", :status => :Unauthorized
-		end
-	end
 	
-	helper_method :current_course
-	def current_course
-		current_section.course
-	end
 
 	def section_params
 		params.require(:section).permit(:title, :row_order_position)
