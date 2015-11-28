91503dbb09f7dd5e3c28acb06a287a6c8ab2d44d
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-11-28 23:32:12 +0000

    next lesson button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,27 @@ class Lesson < ActiveRecord::Base
 
 	include RankedModel
 	ranks :row_order, :with_same => :section_id
+
+	def next_lesson
+		lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+		if lesson.blank? && section.next_section
+			return section.next_section.lessons.rank(:row_order).first
+		end
+	end
 end
+=begin
+	
+section loads the section connected to the lesson we are in the context of
+
+section.lessons looks at the section of the lesson we are in the context of and
+loads all the lessons within that section 
+
+where allows us to add snippets of SQL code that can filter records and remove
+items that don't have certain properties yet. filters out all the lessons
+that are not after the current lesson in the section
+
+rank(:row_order) sorts the results by the row_order
+
+=end
+
+
