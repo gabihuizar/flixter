97c0a7be7fe07e927c051a2dd3aae8b98d2cfa89
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-11-29 05:09:10 +0000

    flixter done.. for now

--- test/controllers/instructor/sections_controller_test.rb
+++ test/controllers/instructor/sections_controller_test.rb
@@ -4,23 +4,6 @@ class Instructor::SectionsControllerTest < ActionController::TestCase
 
 
 
-  test "create new section" do
-        user = FactoryGirl.create(:user)
-        sign_in user
-
-        course = FactoryGirl.create(:course, :user => user)
-
-        assert_difference 'course.sections.count' do
-            post :create, :course_id => course.id, section: { title: "Chapter 1" } 
-        end
-
-        assert_redirected_to instructor_course_path(course.id)
-        assert_equal 1, course.sections.count
-
-    end
-
-
-
   test "new section creation fails if not signed_in" do
   	@course = FactoryGirl.create(:course)
 
